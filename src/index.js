27a9157c13677d6eb8b4f5fbb10cc310e79ceece
LuXoN1999 <llovretic99@gmail.com>
2022-06-14 21:38:19 +0200

    Entire website structure changed

--- src/index.js
+++ src/index.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './Style/MainStyle.css';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import LogInHeader from './Pages/LogIn/Header/Header';
-import LogInMain from './Pages/LogIn/Main/Main';
-import RegisterHeader from './Pages/Register/Header/Header';
-import RegisterMain from './Pages/Register/Main/Main';
-import FaceRecognizerHeader from './Pages/FaceRecognizer/Header'
-import FaceRecognizerMain from './Pages/FaceRecognizer/Main';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
 
-import './Style/Fonts/Fonts.css';
-import reportWebVitals from './reportWebVitals';
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
